b86f194362d4c81e78760b39168ba09bef606e82
Coen Jones <Coenmcj@gmail.com>
2017-02-23 20:54:37 +1100

    Update Creature.java

--- Creature.java
+++ Creature.java
@@ -5,14 +5,19 @@ public class Creature
 	private int armour;
 	private int attack;
 	private int evasion;
+	private String name; //Added this since it's not? going off Array
 	
-	public Creature(int max_health, int health, int armour, int attack, int evasion)
+	// Can't just be a "creature" lmao
+	
+	public Creature(int max_health, int health, int armour, int attack, int evasion, String name)
 	{
 		this.max_health = max_health;
 		this.health = health;
 		this.armour = armour;
 		this.attack = attack;
 		this.evasion = evasion;
+		this.name = name;
 	}
+	
 	
 }
